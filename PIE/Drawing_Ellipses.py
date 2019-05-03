0105477cdba9653398ef8dd74d420f2263d27930
xssad <50161424+xssad@users.noreply.github.com>
2019-05-03 19:29:26 +0800

    Add files via upload

---
+++ PIE/Drawing_Ellipses.py
@@ -0,0 +1,21 @@
+import pygame
+from pygame.locals import *
+pygame.init()
+screen = pygame.display.set_mode((600,500))
+pygame.display.set_caption("Drawing Ellipses")
+while True:
+    for event in pygame.event.get():
+        if event.type in (QUIT,KEYDOWN):
+            sys.exit()
+
+    screen.fill((0,0,200))
+
+    #draw a circle
+    color = 255,255,0
+    # position = 300,250
+    # radius = 100,50
+    rec = 300,250,150,90
+    width = 10
+    pygame.draw.ellipse(screen, color, rec, width)
+
+    pygame.display.update()
